b25175fa9662c5e66f1d56c878ea5876efa70d20
rahulk <ping2rahulk@gmail.com>
2026-04-29 01:14:45 +0530

    Add placeholder patient details controller

---
+++ Nhs.PatientRegistry/Nhs.PatientRegistry.Api/Controllers/PatientsController.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using Nhs.PatientRegistry.Api.Services;
+
+namespace Nhs.PatientRegistry.Api.Controllers
+{
+    [ApiController]
+    [Route("api/patients")]
+    public class PatientsController : Controller
+    {
+        private readonly IPatientService _patientService;
+        private readonly IValidator<int> _patientIdValidator;
+        private readonly ILogger<PatientsController> _logger;
+
+        public PatientsController(
+            IPatientService patientService,
+            IValidator<int> patientIdValidator,
+            ILogger<PatientsController> logger)
+        {
+            _patientService = patientService;
+            _patientIdValidator = patientIdValidator;
+            _logger = logger;
+        }
+
+        [HttpGet("{id}")]
+        public Task<IActionResult> GetPatientDetailsById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
